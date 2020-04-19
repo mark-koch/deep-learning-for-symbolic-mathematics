1f9a46cbc263b0de3cc3ef4ae8f2afa916736d63
Mark Koch <48097969+mark-koch@users.noreply.github.com>
2020-04-19 20:06:40 +0200

    Use IterableDataset and fix mask

--- data.py
+++ data.py
@@ -1,6 +1,5 @@
 import torch.utils.data
-import torch.utils.data.dataset
-import io
+from torch.utils.data.dataset import IterableDataset
 from vocab import token2id
 
 
@@ -12,69 +11,76 @@ def parse_paper_format(line):
     return f, F
 
 
-class SymbolicIntegrationDataset(torch.utils.data.dataset.Dataset):
-    """ Symbolic integration dataset. """
-    def __init__(self, data_paths, max_input_len=-1, max_target_len=-1):
-        self.data = []
-        self.max_input_len = 0
-        self.max_target_len = 0
+class SymbolicIntegrationDataset(IterableDataset):
+    """
+    Symbolic integration dataset.
+
+    Because of the large data files, this dataset doesn't keep the data in memory. Instead we read the files line by
+    line and alternately yield data from each file. Note that for this reason we cannot shuffle the data. Also this
+    class is set up to work with only a single worker. It does not support multiprocessing.
+    """
+    def __init__(self, data_paths, device, max_input_len=-1, max_target_len=-1):
+        self.data_paths = data_paths
+        self.device = device
+        self.max_input_len = max_input_len
+        self.max_target_len = max_target_len
+        self.files = []
+        self.current_file = 0
         for path in data_paths:
-            with io.open(path, "r", encoding="utf-8") as f:
-                lines = f.readlines()
-                for i, line in enumerate(lines):
-                    f, F = parse_paper_format(line)
-                    f.insert(0, token2id("<sos>"))
-                    F.insert(0, token2id("<sos>"))
-                    f.append(token2id("<eos>"))
-                    F.append(token2id("<eos>"))
-                    if (len(f) > max_input_len > 0) or (len(f) > max_target_len > 0):
-                        continue
-                    self.max_input_len = max(self.max_input_len, len(f))
-                    self.max_target_len = max(self.max_target_len, len(F))
-                    self.data.append((torch.tensor(f), torch.tensor(F)))
+            self.files.append(open(path, "r", encoding="utf-8"))
 
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-        return self.data[idx]
+    def __iter__(self):
+        while True:
+            try:
+                line = next(self.files[self.current_file])
+                inp, tar = parse_paper_format(line)
+                inp.insert(0, token2id("<sos>"))
+                tar.insert(0, token2id("<sos>"))
+                inp.append(token2id("<eos>"))
+                tar.append(token2id("<eos>"))
+                if (len(inp) > self.max_input_len > 0) or (len(tar) > self.max_target_len > 0):
+                    continue
+                yield torch.tensor(inp, device=self.device), torch.tensor(tar, device=self.device)
+                self.current_file = (self.current_file + 1) % (len(self.data_paths) - 1)
+            except StopIteration:
+                # The file has ended. We need to reopen the stream
+                self.files[self.current_file].close()
+                self.files[self.current_file] = open(self.data_paths[self.current_file], "r", encoding="utf-8")
 
 
 def collate_fn(data):
     """
-    Creates mini-batch tensors from (f, F) tuples.
+    Creates mini-batch tensors from (input, target) tuples.
 
     Args:
-        data: List of (f, F) tuples containing the training data.
+        data: Dictionary with padded training data and masks
     """
     max_input_len = max(d[0].shape[0] for d in data)
     max_target_len = max(d[1].shape[0] for d in data)
     batch_size = len(data)
 
     # Prepare zero padding
-    input_padded = torch.zeros(batch_size, max_input_len, dtype=torch.long)
-    target_padded = torch.zeros(batch_size, max_target_len, dtype=torch.long)
+    input_padded = torch.zeros(batch_size, max_input_len, dtype=torch.long, requires_grad=False)
+    target_padded = torch.zeros(batch_size, max_target_len, dtype=torch.long, requires_grad=False)
 
     # Padding masks of shape [batch_size, 1, 1, seq_len]. Empty dimensions are needed for broadcasting in multi-head
     # attention to [..., seq_len_q, seq_len_k].
-    enc_padding_mask = torch.zeros(batch_size, 1, 1, max_input_len, dtype=torch.float)
-    dec_padding_mask = torch.zeros(batch_size, 1, 1, max_target_len, dtype=torch.float)
+    enc_padding_mask = torch.ones(batch_size, 1, 1, max_input_len, dtype=torch.float, requires_grad=False)
+    dec_padding_mask = torch.ones(batch_size, 1, 1, max_target_len, dtype=torch.float, requires_grad=False)
 
     for i, d in enumerate(data):
         input_padded[i, :d[0].shape[0]] = d[0]
         target_padded[i, :d[1].shape[0]] = d[1]
-        enc_padding_mask[i, 0, 0, :d[0].shape[0]] = 1
-        dec_padding_mask[i, 0, 0, :d[1].shape[0]] = 1
+        enc_padding_mask[i, 0, 0, :d[0].shape[0]] = 0
+        dec_padding_mask[i, 0, 0, :d[1].shape[0]] = 0
 
-    # For each position in the decoder dequence, mask out the positions that are in the future. The resulting matrix
+    # For each position in the decoder sequence, mask out the positions that are in the future. The resulting matrix
     # looks like this
     #                                          [[0, 1, 1, 1],
     #                                           [0, 0, 1, 1],
     #                                           [0, 0, 0, 1],
     #                                           [0, 0, 0, 0]]
-    look_ahead_mask = torch.ones(max_target_len, max_target_len, dtype=torch.float).triu(diagonal=1)
+    look_ahead_mask = torch.ones(max_target_len, max_target_len, dtype=torch.float, requires_grad=False).triu(1)
 
     dec_combined_mask = torch.max(dec_padding_mask, look_ahead_mask)
 
